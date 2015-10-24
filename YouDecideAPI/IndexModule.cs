f1ef75620df14812221684cc018e440ec0ada73e
Danny Parkinson <danny.parkinson@laterooms.com>
2015-10-24 17:14:56 +0100

    playing with api stuff storing texts

--- YouDecideAPI/IndexModule.cs
+++ YouDecideAPI/IndexModule.cs
@@ -1,25 +1,73 @@
 ﻿namespace YouDecideAPI
 {
+    using System.Collections.Generic;
+
     using Nancy;
     using Nancy.ModelBinding;
     public class IndexModule : NancyModule
     {
-        private static SmsMessage nastyGlobal = new SmsMessage();
+        private static Dictionary<string,SmsMessage> nastyGlobal = new Dictionary<string, SmsMessage>();
+
+        private static SmsMessage lastMessage = new SmsMessage();
         public IndexModule()
         {
             Get["/"] = parameters =>
-            {
-                return View["index"];
-            };
+                {
+                    return View["index"];
+                };
+
             Get["/status"] = parameters =>
-            {
-                return nastyGlobal.FullMessage();
-            };
-            Get["/inputtext/"] = parameters =>
-            {
-                nastyGlobal = this.Bind<SmsMessage>();
-                return "ok: " + nastyGlobal.FullMessage();
-            };
+                {
+                    return "ok";
+                };
+
+            Get["/storedtexts/{id}"] = parameters =>
+                {
+                    var id = parameters.id;
+                    string response;
+
+                    if (nastyGlobal.ContainsKey(id))
+                    {
+                        response = nastyGlobal[id].FullMessage();
+                    }
+                    else
+                    {
+                        response = "No message stored for id of " + id;
+                    }
+
+                    return response;
+                };
+
+            Get["/lastmessage"] = parameters =>
+                {
+                    return lastMessage.FullMessage();
+                };
+
+            Get["/listallstoredtextsids/"] = parameters =>
+                {
+                    var keys = nastyGlobal.Keys;
+                    var response = "";
+                    foreach (var key in keys)
+                    {
+                        response += key.ToString()+"\n";
+                    }
+                    return response;
+                };
+
+
+            Get["/inputsms/"] = parameters =>
+                {
+                    var smsMessage = this.Bind<SmsMessage>();
+                    lastMessage = smsMessage;
+                    var response = "Key already exists";
+                    if (!nastyGlobal.ContainsKey(smsMessage.Id))
+                    {
+                        nastyGlobal.Add(smsMessage.Id, smsMessage);
+                        response = smsMessage.FullMessage();
+                    }
+                    return response;
+                };
+            
         }
     }
 }
