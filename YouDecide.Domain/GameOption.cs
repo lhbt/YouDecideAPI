8a6f3ee9ebbf777a67f8e184e4f8c2e378105ef9
unknown <csudbery@dltd00ym32.ad.laterooms.com>
2015-10-24 17:41:09 +0100

    Now we have a gamestate endpoint.

---
+++ YouDecide.Domain/GameOption.cs
@@ -0,0 +1,8 @@
+namespace YouDecide.Domain
+{
+    public sealed class GameOption
+    {
+        public int OptionNumber { get; set; }
+        public string Option { get; set; }
+    }
+}
---
+++ YouDecideAPI/Modules/GameStateModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using YouDecide.Domain;
+
+namespace YouDecideAPI.Modules
+{
+    public class GameStateModule : NancyModule
+    {
+        public GameStateModule(IStoryNavigator storyNavigator)
+        {
+            Get["/gamestate"] = parameters =>
+            {
+                return Response.AsJson(new GameState
+                    {
+                        GameOptions = new List<GameOption>
+                            {
+                                new GameOption
+                                    {
+                                        OptionNumber = 1,
+                                        Option = "Run away from lollipop-bearing skeletons."
+                                    },
+                                new GameOption
+                                    {
+                                        OptionNumber = 1,
+                                        Option = "Eat cake!"
+                                    },
+                                new GameOption
+                                    {
+                                        OptionNumber = 1,
+                                        Option = "Go home and have a bath."
+                                    }
+                            }
+                    });
+            };
+        }
+    }
+}
