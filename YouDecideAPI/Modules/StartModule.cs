c5750313342ba3bda612fc71601aab60cd13cfff
unknown <csudbery@dltd00ym32.ad.laterooms.com>
2015-10-24 13:17:31 +0100

    Added start module

---
+++ YouDecideAPI/Modules/StartModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace YouDecideAPI.Modules
+{
+    public class StartModule : NancyModule
+    {
+        public StartModule()
+        {
+            Get["/start"] = parameters =>
+            {
+                string textSent = Request.Query["text"];
+                return textSent;
+            };
+        }
+    }
+}
