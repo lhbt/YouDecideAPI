00d1b42e5bda0ac1702223cf45d9a3dbc2758880
Danny Parkinson <danny.parkinson@laterooms.com>
2015-10-24 14:04:50 +0100

    added nasty global to test texting

--- YouDecideAPI/IndexModule.cs
+++ YouDecideAPI/IndexModule.cs
@@ -4,11 +4,22 @@
 
     public class IndexModule : NancyModule
     {
+        private static string nastyGlobal;
         public IndexModule()
         {
             Get["/"] = parameters =>
             {
                 return View["index"];
+            };
+            Get["/status"] = parameter =>
+            {
+                return nastyGlobal;
+            };
+            Get["/inputtext/"] = parameter =>
+            {
+                var text = this.Request.Query;
+                nastyGlobal = text.id + " " + text.to + " " + text.from + " " + text.keyword + " " + text.content;
+                return "ok";
             };
         }
     }
